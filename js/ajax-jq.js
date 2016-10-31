777419a29836ad5a7dcc8b6025e2f3046b8c7b27
yanhaijing <yanhaijing@yeah.net>
2016-10-31 08:21:51 +0800

    feat: 优化jq-ajax

--- js/ajax-jq.js
+++ js/ajax-jq.js
@@ -1,9 +1,9 @@
 ES6Promise.polyfill();
 
 function ajax(opt) {
-    return new Promise(function (resolve, reject) {
-        $.ajax(opt).done(resolve).fail(reject)
-    });
-    // return Promise.resolve($.ajax(opt));
+    // return new Promise(function (resolve, reject) {
+    //     $.ajax(opt).done(resolve).fail(reject)
+    // });
+    return Promise.resolve($.ajax(opt));
 }
 
